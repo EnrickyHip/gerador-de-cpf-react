31716b6b4f062b469464fa22e69ee28704cbaedb
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-04-11 16:56:33 -0300

    Criando html e css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>GERAR / VALIDAR CPF</h1>
       </header>
+      <main className="App-main">
+
+        <form>
+
+          <div className='flex'>
+            <input className='full-input'></input>
+            <button type="button" className='btn'>Gerar CPF</button>
+            <button type="button" className='btn'>Validar CPF</button>
+          </div>
+
+        </form>
+
+      </main>
     </div>
   );
 }
