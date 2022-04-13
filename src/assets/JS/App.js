6b669e6c42138f9b62ec61484ecd6edcda738884
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-04-13 15:42:00 -0300

    Adicionando funções de validação e geração de cpf's no App.js, corrigindo alteração de valor do input e mensagem de validação

--- src/assets/JS/App.js
+++ src/assets/JS/App.js
@@ -1,29 +1,72 @@
 import '../CSS/App.css';
 import { Component} from 'react';
+import Cpf from "../../modules/Cpf";
 
 class App extends Component {
 
   state = {
-    textInput: ''
+    cpfInput: '',
+    cpfMessage: ''
   }
 
+  generate = () => {
+    const cpf = Cpf.generate();
+    this.setState({cpfInput : cpf})
+  }
+
+  validate = () => {
+    let message;
+    const cpfInput = document.querySelector("#cpf-input").value;
+    const valid = Cpf.validate(cpfInput);
+
+    if (valid) message = "CPF válido"
+    if (!valid) message = "CPF inválido"
+
+    this.setState({
+      cpfMessage: message
+    });
+
+  }
+
+  onTodoChange = value => {
+    this.setState({
+      cpfInput: value
+    });
+}
+
   render() {
-    const {textInput} = this.state;
+    const {cpfMessage} = this.state;
+    const {cpfInput} = this.state;
 
     return (
-      <div className="App">
+      <div className="app">
         
-        <header className="App-header">
+        <header>
           <h1>GERAR / VALIDAR CPF</h1>
         </header>
 
-        <main className="App-main">
+        <main>
           <form>
 
-            <div className='flex'>
-              <input className='full-input' placeholder='Digite um CPF' value={textInput}></input>
-              <button type="button" className='btn'>Gerar CPF</button>
-              <button type="button" className='btn'>Validar CPF</button>
+            <div>
+
+              <input
+                id = 'cpf-input' 
+                className = 'full-input' 
+                placeholder = 'Digite um CPF' 
+                autoComplete='off'
+                value = { cpfInput }
+                maxLength = "14"
+                onChange = {e => this.onTodoChange(e.target.value)}> 
+                {/* esse atributo serve para ser possível a alteração do value do input quando o usuário digitar */}
+              </input>
+
+              <button onClick = { this.generate } type="button" className='btn'>Gerar CPF</button>
+
+              <button onClick = { this.validate } type="button" className='btn'>Validar CPF</button>
+
+              <p id='cpf-message'> { cpfMessage } </p>
+
             </div>
 
           </form>
--- src/modules/Cpf.js
+++ src/modules/Cpf.js
@@ -1,4 +1,4 @@
- import {random} from "./random.js";
+ import random from "./random.js";
  
  export default class Cpf {
     
