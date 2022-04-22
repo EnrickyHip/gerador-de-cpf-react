c91951ce8ea41d32ee9914a988f4f9146ea5f529
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-04-21 22:36:49 -0300

    criando testes para o componente Button

---
+++ src/components/Button/Button.spec.jsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event";
+import { Button } from '.'
+
+describe("<Button />", () => {
+
+  it("should render the button with the text 'Test'", () => {
+    render(<Button text = "Test" />);
+    const button = screen.getByRole("button", { name: /test/i});
+
+    expect(button).toBeInTheDocument();
+  })
+
+  /* -------------------------------- */
+
+  it("should call function onClick", () => {
+    const fn = jest.fn();
+    render(<Button text = "Test" onClick = {fn} />);
+
+    const button = screen.getByRole("button", { name: /test/i});
+    userEvent.click(button)
+
+    expect(fn).toBeCalled();
+  })
+
+  /* -------------------------------- */
+
+  it("should match snapshot", () => {
+    const fn = jest.fn();
+    const { container } = render(<Button text = "Test" onClick = {fn} />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  })
+})
