6a469829d71741f66fea29bb9752820e6325784f
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-04-27 17:26:38 -0300

    instalando mock service e criando testes para os componentes Home e HomeMain

---
+++ src/components/HomeMain/HomeMain.spec.jsx
@@ -0,0 +1,44 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { HomeMain } from ".";
+
+describe("<Button />", () => {
+  it("should render input, generate button and validate button", () => {
+    render(<HomeMain />);
+
+    const input = screen.getByPlaceholderText(/digite um cpf/i);
+    const generateButton = screen.getByRole("button", { name: /gerar cpf/i });
+    const validateButton = screen.getByRole("button", { name: /validar cpf/i });
+
+    expect(input).toBeInTheDocument();
+    expect(generateButton).toBeInTheDocument();
+    expect(validateButton).toBeInTheDocument();
+  });
+
+  it("should generate CPF", () => {
+    render(<HomeMain />);
+
+    const input = screen.getByPlaceholderText(/digite um cpf/i);
+    const generateButton = screen.getByRole("button", { name: /gerar cpf/i });
+
+    expect(input.value).toBe("");
+
+    userEvent.click(generateButton);
+
+    expect(input.value).not.toBe("");
+  });
+
+  it("should validate CPF", () => {
+    render(<HomeMain />);
+
+    const input = screen.getByPlaceholderText(/digite um cpf/i);
+    const validateButton = screen.getByRole("button", { name: /validar cpf/i });
+
+    userEvent.type(input, "455.185.758-07");
+    expect(input.value).toBe("455.185.758-07");
+
+    userEvent.click(validateButton);
+    const message = screen.getByText("CPF válido");
+    expect(message).toBeInTheDocument();
+  });
+});
--- src/components/HomeMain/index.jsx
+++ src/components/HomeMain/index.jsx
@@ -1,59 +1,55 @@
-import "./style.css"
+import "./style.css";
 
 import { Component } from "react";
 import { Button } from "../Button";
 import { Input } from "../Input";
 
 import Cpf from "../../modules/Cpf";
-import { getValidateMessage } from '../../functions/getValidateMessage';
+import { getValidateMessage } from "../../functions/getValidateMessage";
 import { Message } from "../Message";
 
 export class HomeMain extends Component {
-
   state = {
-    cpfMessage: '',
-    cpfInput: '',
-  }
+    cpfMessage: "",
+    cpfInput: "",
+  };
 
   handleGenerate = () => {
     const cpf = Cpf.generate();
-    this.setState({cpfInput : cpf})
-  }
+    this.setState({ cpfInput: cpf });
+  };
 
   handleValidate = () => {
     const message = getValidateMessage();
     this.setState({
       cpfMessage: message,
     });
-  }
+  };
 
-  handleChange = event => {
-    const {value} = event.target
+  handleChange = (event) => {
+    const { value } = event.target;
 
     this.setState({
       cpfInput: value,
     });
-  }
+  };
 
-  render(){
-    const {cpfMessage, cpfInput} = this.state;
+  render() {
+    const { cpfMessage, cpfInput } = this.state;
 
     return (
       <main>
         <form>
           <div>
-
-            <Input textInput={cpfInput} handleChange={this.handleChange}/>
+            <Input textInput={cpfInput} handleChange={this.handleChange} />
 
             <Button text="Gerar CPF" onClick={this.handleGenerate} />
             <Button text="Validar CPF" onClick={this.handleValidate} />
 
             <Message message={cpfMessage} />
-
           </div>
         </form>
       </main>
-    )
+    );
   }
 }
-
